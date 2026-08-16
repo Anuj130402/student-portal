c559f6debdc7dd47a413baa642ccdd0d3f50db1d
Anuj Kandwal <anujkandwal@Anujs-MacBook-Air.local>
2026-08-16 10:50:43 +0530

    Set default mode to recall and tighten the source limit

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -1,5 +1,5 @@
 const SUMMARIZE_URL =
   "http://127.0.0.1:5001/demo-student-portal/us-central1/summarize";
 
-const DEFAULT_MODE = "short";
-const MAX_SOURCES = 5;
+const DEFAULT_MODE = "recall";
+const MAX_SOURCES = 3;
